5f4adc3e9066522ca6e011a3ef66310bc4c4df22
Luai Salah <luaisalah9@gmail.com>
2023-04-16 21:41:29 +0200

    ProjectFiles

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace AniyomiEpisodesExtractor
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No Argumants provided, so no action will be taken!");
+                return;
+            }
+
+            if (args[0] != "-c")
+                return;
+
+            var directory = new DirectoryInfo(Directory.GetCurrentDirectory());
+
+            int filesMoved = 0;
+
+
+            foreach (var dir in directory.GetDirectories())
+            {
+                foreach (var file in dir.GetFiles("*.mp4"))
+                {
+                    file.MoveTo($"{directory.FullName}/{file.Name}");
+                    Console.WriteLine($"{directory.FullName}/{file.Name}");
+                    filesMoved++;
+                    dir.Delete(true);
+                }
+            }
+
+            Console.WriteLine($"\n{filesMoved} files were moved!");
+        }
+    }
+}
