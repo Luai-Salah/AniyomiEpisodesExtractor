0b4d10133bddca0896e792ae30ae4f686112a2f5
Luai Omer <luaidev@pm.me>
2025-03-20 20:48:49 +0530

    Project migrate to .NET Core and added a linux build
    - Created a new project with .NET 9 instead of .Netframework 4.8
    - Added a compound build for Windows and Linux builds

---
+++ aee/Program.cs
@@ -0,0 +1,61 @@
+﻿if (args.Length == 0)
+{
+    Console.WriteLine("No option was specified, the only option available is '-c' for current directory...");
+    Console.ReadKey();
+    return;
+}
+
+if (args[0] != "-c")
+{
+    Console.WriteLine($"the option '{args[0]}' isn't known, the only option available is '-c' for current directory...");
+    Console.ReadKey();
+    return;
+}
+
+var directory = new DirectoryInfo(Directory.GetCurrentDirectory());
+int filesMoved = 0;
+
+var filesToBeMoved = new List<(FileInfo, DirectoryInfo)>();
+
+foreach (var dir in directory.GetDirectories())
+{
+    foreach (var file in dir.GetFiles("*.*").Where(s => s.Extension is ".mp4" or ".mkv"))
+    {
+        filesToBeMoved.Add((file, dir));
+        Console.WriteLine($"{directory.FullName}/{file.Name}");
+    }
+}
+
+if (filesToBeMoved.Count == 0) {
+    Console.WriteLine("No episodes found to be moved");
+    Console.ReadKey();
+    return;
+}
+
+Console.WriteLine("\nThe following episodes will be processed... do you want to proceed? [Y/N]");
+
+KeyInput:
+
+var answer = Console.ReadLine();
+if (answer is "n" or "N")
+{
+    Console.WriteLine("Operation canceled.");
+    return;
+}
+
+if (answer != "y" && answer != "Y")
+{
+    Console.WriteLine("Please enter either Y (yes) or N (no)");
+    goto KeyInput;
+}
+
+foreach (var (file, parent) in filesToBeMoved)
+{
+    file.MoveTo($"{directory.FullName}/{file.Name}");
+    filesMoved++;
+    parent.Delete(true);
+}
+
+Console.WriteLine($"\n{filesMoved} episodes were moved!");
+Console.ReadKey();
+ 
